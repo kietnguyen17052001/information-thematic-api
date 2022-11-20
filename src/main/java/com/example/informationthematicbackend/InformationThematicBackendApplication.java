a5bf6b895e1caa8d9fd2d4ad4c0fd4da5302692f
ngdangkiet <kietnguyen17052001@gmail.com>
2022-11-20 21:00:06 +0700

    init project

---
+++ src/main/java/com/example/informationthematicbackend/InformationThematicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.informationthematicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationThematicBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformationThematicBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/informationthematicbackend/InformationThematicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.informationthematicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationThematicBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
