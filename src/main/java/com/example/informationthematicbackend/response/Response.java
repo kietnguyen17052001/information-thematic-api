41996febb8e4bff1e45e75bdb3cd010955829567
ngdangkiet <kietnguyen17052001@gmail.com>
2022-12-03 11:14:56 +0700

    feat: common

---
+++ src/main/java/com/example/informationthematicbackend/response/Response.java
@@ -0,0 +1,21 @@
+package com.example.informationthematicbackend.response;
+
+import com.example.informationthematicbackend.model.dto.ErrorDTO;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder(setterPrefix = "set")
+public class Response<T> implements Serializable {
+    private Boolean success;
+    private String message;
+    private List<ErrorDTO> errorDTOs;
+    private T data;
+    private Timestamp timestamp;
+}
